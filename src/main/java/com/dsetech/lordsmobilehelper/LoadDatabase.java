8d3fb647f130f7d990eed7411453ad66646b02fb
Darren S. Eyers <dseyers@gmail.com>
2020-07-26 17:17:31 +0100

    DB Player configuration added to add record to db when application runs

---
+++ src/main/java/com/dsetech/lordsmobilehelper/LoadDatabase.java
@@ -0,0 +1,21 @@
+package com.dsetech.lordsmobilehelper;
+
+import com.dsetech.lordsmobilehelper.model.Player;
+import com.dsetech.lordsmobilehelper.repo.PlayerRepo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@Slf4j
+class LoadDatabase {
+
+    @Bean
+    CommandLineRunner initDatabase(PlayerRepo playerRepo) {
+        return args -> {
+            log.info("Preloading " + playerRepo.save(new Player().setName("Vqmpire")));
+            log.info("Preloading " + playerRepo.save(new Player().setName("Lore")));
+        };
+    }
+}
