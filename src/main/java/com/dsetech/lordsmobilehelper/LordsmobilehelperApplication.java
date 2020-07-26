3a3b83f52cec28c89015fd2d01dfdef92ac3667f
Darren S. Eyers <dseyers@gmail.com>
2020-07-26 14:52:26 +0100

    Generated code from Spring Initializr and extended .gitignore file for InteliJ, junk, build

---
+++ src/main/java/com/dsetech/lordsmobilehelper/LordsmobilehelperApplication.java
@@ -0,0 +1,13 @@
+package com.dsetech.lordsmobilehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LordsmobilehelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LordsmobilehelperApplication.class, args);
+	}
+
+}
